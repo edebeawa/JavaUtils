a80b8cbc6a8261ec790bb0f0a8aa7b56040b2189
edebe <3614332960@qq.com>
2023-12-27 18:02:27 +0800

    Initial commit

--- src/main/java/pers/edebe/util/base/VirtualMachineUtils.java
+++ src/main/java/pers/edebe/util/base/VirtualMachineUtils.java
@@ -1,50 +1,23 @@
 package pers.edebe.util.base;
 
-import org.jetbrains.annotations.Nullable;
 import pers.edebe.util.wrapper.ClassWrapper;
+import pers.edebe.util.wrapper.FieldWrapper;
 
 public class VirtualMachineUtils {
-    public static void allowAttachSelf(Class<?> clazz, boolean value) throws ReflectiveOperationException {
-        ClassWrapper.wrap(clazz)
-                .getDeclaredField("ALLOW_ATTACH_SELF")
-                .setAccessibleNoRestrict(true)
-                .setType(boolean.class)
-                .setStatic(value);
+    private static final FieldWrapper<Boolean> HOT_SPOT_VIRTUAL_MACHINE_ALLOW_ATTACH_SELF_FIELD;
+
+    static {
+        try {
+            HOT_SPOT_VIRTUAL_MACHINE_ALLOW_ATTACH_SELF_FIELD = ClassWrapper.wrap(Class.forName("sun.tools.attach.HotSpotVirtualMachine"))
+                    .getDeclaredField("ALLOW_ATTACH_SELF")
+                    .setAccessibleNoRestrict(true)
+                    .setType(boolean.class);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void allowAttachSelf(boolean value) throws ReflectiveOperationException {
-        allowAttachSelf(Class.forName("sun.tools.attach.HotSpotVirtualMachine"), value);
-    }
-
-    public static Object attach(Class<?> clazz, long pid) throws ReflectiveOperationException {
-        return clazz.getMethod("attach", String.class).invoke(null, String.valueOf(pid));
-    }
-
-    public static void detach(Class<?> clazz, Object instance) throws ReflectiveOperationException {
-        clazz.getMethod("detach").invoke(instance);
-    }
-
-    public static void loadAgentLibrary(Class<?> clazz, Object instance, String agent, @Nullable String argument) throws ReflectiveOperationException {
-        clazz.getMethod("loadAgentLibrary", String.class, String.class).invoke(instance, agent, argument);
-    }
-
-    public static void loadAgentLibrary(Class<?> clazz, Object instance, String agent) throws ReflectiveOperationException {
-        loadAgentLibrary(clazz, instance, agent, null);
-    }
-
-    public static void loadAgentPath(Class<?> clazz, Object instance, String agent, @Nullable String argument) throws ReflectiveOperationException {
-        clazz.getMethod("loadAgentPath", String.class, String.class).invoke(instance, agent, argument);
-    }
-
-    public static void loadAgentPath(Class<?> clazz, Object instance, String agent) throws ReflectiveOperationException {
-        loadAgentPath(clazz, instance, agent, null);
-    }
-
-    public static void loadAgent(Class<?> clazz, Object instance, String agent, @Nullable String argument) throws ReflectiveOperationException {
-        clazz.getMethod("loadAgent", String.class, String.class).invoke(instance, agent, argument);
-    }
-
-    public static void loadAgent(Class<?> clazz, Object instance, String agent) throws ReflectiveOperationException {
-        loadAgent(clazz, instance, agent, null);
+        HOT_SPOT_VIRTUAL_MACHINE_ALLOW_ATTACH_SELF_FIELD.setStatic(value);
     }
 }
