930e1b0322f93ea68bd1872a7cd4ae54a384124d
edebe <3614332960@qq.com>
2024-01-01 04:59:32 +0800

    Initial commit

---
+++ src/main/java/pers/edebe/util/base/AccessUtils.java
@@ -0,0 +1,18 @@
+package pers.edebe.util.base;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccessUtils {
+    public static List<Integer> resolve(int access, int max) {
+        List<Integer> list = new ArrayList<>();
+        while (max > 0) {
+            if (access >= max) {
+                list.add(max);
+                access -= max;
+            }
+            max /= 2;
+        }
+        return list;
+    }
+}
