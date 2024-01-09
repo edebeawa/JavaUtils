c5fd5760a2718c00fd8fcd89d7f9d3f3e3973fe2
edebe <3614332960@qq.com>
2024-01-09 17:10:01 +0800

    Initial commit

--- src/main/java/pers/edebe/util/base/StringUtils.java
+++ src/main/java/pers/edebe/util/base/StringUtils.java
@@ -1,6 +1,6 @@
 package pers.edebe.util.base;
 
-import java.util.Locale;
+import java.util.*;
 
 public class StringUtils {
     public static String toLowerCase(String string) {
@@ -9,5 +9,29 @@ public class StringUtils {
 
     public static String toUpperCase(String string) {
         return string.toUpperCase(Locale.ROOT);
+    }
+
+    public static String toFirstLetterUpperCase(String string) {
+        return toUpperCase(string.substring(0, 1)) + string.substring(1);
+    }
+
+    public static String toHumpForUnderline(String string) {
+        List<String> list = new ArrayList<>(List.of(string.split("")));
+        Set<Integer> set = new HashSet<>();
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            String cursor = list.get(i);
+            int next = i + 1;
+            if (cursor.equals("_") && next != size) {
+                list.set(i, "");
+                list.set(next, toUpperCase(list.get(next)));
+                set.add(next);
+            } else if (!cursor.equals("") && !set.contains(i)) {
+                list.set(i, toLowerCase(cursor));
+            }
+        }
+        StringBuilder builder = new StringBuilder();
+        list.forEach(builder::append);
+        return builder.toString();
     }
 }
