52909c716c015053ea51404d63a9a74da5e3ca4a
edebe <3614332960@qq.com>
2024-03-24 16:08:42 +0800

    Initial commit

---
+++ src/main/java/pers/edebe/util/exception/ResourceNotFoundException.java
@@ -0,0 +1,21 @@
+package pers.edebe.util.exception;
+
+import java.io.IOException;
+
+public class ResourceNotFoundException extends IOException {
+    public ResourceNotFoundException() {
+        super();
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
