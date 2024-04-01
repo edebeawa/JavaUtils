6ea195667b1c47edfeb7d387fc139d08e211008c
edebe <3614332960@qq.com>
2024-04-01 10:53:44 +0800

    Initial commit

---
+++ src/main/java/pers/edebe/util/base/StringBuilderUtils.java
@@ -0,0 +1,23 @@
+package pers.edebe.util.base;
+
+public class StringBuilderUtils {
+    public static StringBuilder clear(StringBuilder builder) {
+        return builder.delete(0, builder.length());
+    }
+
+    public static String toStringAndClear(StringBuilder builder) {
+        String string = builder.toString();
+        clear(builder);
+        return string;
+    }
+
+    public static StringBuffer clear(StringBuffer buffer) {
+        return buffer.delete(0, buffer.length());
+    }
+
+    public static String toStringAndClear(StringBuffer buffer) {
+        String string = buffer.toString();
+        clear(buffer);
+        return string;
+    }
+}
