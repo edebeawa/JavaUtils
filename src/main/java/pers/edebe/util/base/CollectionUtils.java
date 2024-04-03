24d59b28abb318ab5c19b838583eca55a4e56fb8
edebe <3614332960@qq.com>
2024-04-04 07:36:38 +0800

    Initial commit

--- src/main/java/pers/edebe/util/base/CollectionUtils.java
+++ src/main/java/pers/edebe/util/base/CollectionUtils.java
@@ -3,12 +3,20 @@ package pers.edebe.util.base;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class CollectionUtils {
     @SafeVarargs
     public static <T> List<T> merge(Iterable<? extends T>... iterables) {
         List<T> list = new ArrayList<>();
         Arrays.stream(iterables).forEach((iterable) -> iterable.forEach(list::add));
+        return list;
+    }
+
+    @SafeVarargs
+    public static <T> List<T> merge(Stream<? extends T>... streams) {
+        List<T> list = new ArrayList<>();
+        Arrays.stream(streams).forEach((stream) -> stream.forEach(list::add));
         return list;
     }
 }
