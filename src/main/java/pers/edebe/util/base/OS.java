e14b0f422a129bdb4f5ec81eaddd4f82f96d6117
edebe <3614332960@qq.com>
2024-07-27 09:42:14 +0800

    Initial commit

--- src/main/java/pers/edebe/util/base/OS.java
+++ src/main/java/pers/edebe/util/base/OS.java
@@ -1,16 +1,20 @@
 package pers.edebe.util.base;
 
+import java.util.Locale;
+
 public enum OS {
-    WINDOWS, LINUX, MACINTOSH, UNKNOWN;
+    WINDOWS, LINUX, MACINTOSH, SOLARIS, UNKNOWN;
 
     public static OS get() {
-        String name = System.getProperty("os.name");
-        if (name.contains("Windows"))
+        String name = System.getProperty("os.name").toLowerCase(Locale.ROOT);
+        if (name.contains("win"))
             return WINDOWS;
-        if (name.contains("Linux"))
+        if (name.contains("linux") || name.contains("unix"))
             return LINUX;
-        if (name.contains("Mac") || name.contains("OS X"))
+        if (name.contains("mac") || name.contains("os x"))
             return MACINTOSH;
+        if (name.contains("solaris") || name.contains("sunos"))
+            return SOLARIS;
         return UNKNOWN;
     }
 }
--- src/main/java/pers/edebe/util/base/ProcessUtils.java
+++ src/main/java/pers/edebe/util/base/ProcessUtils.java
@@ -1,11 +1,26 @@
 package pers.edebe.util.base;
 
 import lombok.experimental.UtilityClass;
+import pers.edebe.util.collect.ImmutableList;
 
+import java.io.File;
 import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
 
 @UtilityClass
 public class ProcessUtils {
+    public static ProcessBuilder fileExecuteBuilder(File file, List<String> command) {
+        List<String> list = new ArrayList<>();
+        list.add(file.getAbsolutePath());
+        list.addAll(command);
+        return new ProcessBuilder(list).directory(file.getParentFile());
+    }
+
+    public static ProcessBuilder fileExecuteBuilder(File file, String... command) {
+        return fileExecuteBuilder(file, new ImmutableList<>(command));
+    }
+
     public static long getCurrentProcessId() {
         String name = ManagementFactory.getRuntimeMXBean().getName();
         return Long.parseLong(name.substring(0, name.indexOf("@")));
