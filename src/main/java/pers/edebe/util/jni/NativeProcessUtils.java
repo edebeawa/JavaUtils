2417da665e433ed9853b7c53c21bdd2291726123
edebeawa <104586295+edebeawa@users.noreply.github.com>
2024-09-10 09:45:11 +0800

    Delete src/main directory

--- src/main/java/pers/edebe/util/jni/NativeProcessUtils.java
+++
@@ -1,7 +0,0 @@
-package pers.edebe.util.jni;
-
-public class NativeProcessUtils extends EdebeUtilsNative {
-    public static native void writeProcessMemory(long pid, long address, long value);
-
-    public static native long readProcessMemory(long pid, long address);
-}
